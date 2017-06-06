22b1a4b4458f05279126cfc27ff3295b6c713c5d
Sweenu <brunoinec@gmail.com>
2017-06-06 13:45:48 +0200

    some more refactoring

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
 	$('#Partie_2_1 button').click(function() {
-		$("#Partie_3_1").show();
-		$("#Partie_3_2").hide();
+		$("#Partie_3_1").show()
+		$("#Partie_3_2").hide()
 
         var offerer = new Person($('#name').val(),
                                    $('#address').val(),
-                                   $('#mail').val());
-        offerers.push(offerer);
+                                   $('#mail').val())
+        offerers.push(offerer)
 
         product = new Product($('#product').val(),
                               $('#day').val(),
@@ -16,76 +16,77 @@ $(document).ready(function(){
                               $('#hour_start').val(),
                               $('#min_start').val(),
                               $('#hour_end').val(),
-                              $('#min_end').val());
+                              $('#min_end').val())
 
-        offerer.ajoutProd(product);
+        offerer.ajoutProd(product)
 
-		$('#name_offerer').text(offerer.name);
-		$('#address_offerer').text(offerer.address);
-		$('#name_product').text(offerer.product);
-		$('#day_avail').text(offerer.day_avail);
-		$('#month_avail').text(offerer.month_dispo);
-		$('#year_avail').text(offerer.year_dispo);
-		$('#hour_start_avail').text(offerer.debut_heure_dispo);
-		$('#minute_start_avail').text(offerer.debut_minute_dispo);
-		$('#hour_end_avail').text(offerer.fin_heure_dispo);
-		$('#min_end_avail').text(offerer.fin_minute_dispo);
+		$('#name_offerer').text(offerer.name)
+		$('#address_offerer').text(offerer.address)
+		$('#name_product').text(offerer.product)
+		$('#day_avail').text(offerer.day_avail)
+		$('#month_avail').text(offerer.month_dispo)
+		$('#year_avail').text(offerer.year_dispo)
+		$('#hour_start_avail').text(offerer.debut_heure_dispo)
+		$('#minute_start_avail').text(offerer.debut_minute_dispo)
+		$('#hour_end_avail').text(offerer.fin_heure_dispo)
+		$('#min_end_avail').text(offerer.fin_minute_dispo)
 
-        clear_list("#list_product");
-        print_list("#list_product");
-	});
+        clear_list("#list_product")
+        print_list("#list_product")
+	})
 
 	$('#Partie_2_2 button').click(function() {
-		$("#Partie_3_2").show();
-		$("#Partie_3_1").hide();
+		$("#Partie_3_2").show()
+		$("#Partie_3_1").hide()
 
-        remove_item('#list_product');
-        clear_list("#list_product");
-        print_list("#list_product");
-	});
-});
+        remove_item('#list_product')
+        clear_list("#list_product")
+        print_list("#list_product")
+	})
+})
 
 function Product(name, day, month, year, hour, min, hour_end, min_end) {
-    this.name = name;
-    this.debut = Date(year, month, day, hour, min);
-    this.fin = Date(year, month, day, hour_end, min_end);
+    this.name = name
+    this.debut = Date(year, month, day, hour, min)
+    this.fin = Date(year, month, day, hour_end, min_end)
 }
 
 function Person(name, address, mail) {
-    this.name = name;
-    this.address = address;
-    this.mail = mail;
+    this.name = name
+    this.address = address
+    this.mail = mail
 
-    this.products = [];
+    this.products = []
 
     this.ajoutProd = function(product) {
-        this.products.push(product);
+        this.products.push(product)
     }
 }
 
 function clear_list(blocID) {
-    $(blocID).html('');
+    $(blocID).html('')
 }
 
 function print_list(blocID) {
     for (let i = 0; i < offerers.length; i++) {
         for (let j = 0; j < offerers[i].products.length; j++) {
-            $(blocID).append("<input type='radio' name='product' value='"+ offerers[i].products[j].name + "'>"
+            $(blocID).append("<input type='radio' name='product' value='"
+                             +offerers[i].products[j].name + "'>"
                              + offerers[i].products[j].name
-                             + "<br>");
+                             + "<br>")
         }
     }
 }
 
 function remove_item(blocID) {
-    var nameProduct = $(blocID + ' input:checked').val();
+    var nameProduct = $(blocID + ' input:checked').val()
     for (let i = 0; i < offerers.length; i++) {
         for (let j = 0; j < offerers[i].products.length; j++) {
             if (offerers[i].products[j].name == nameProduct) {
-                offerers[i].products.splice(j, 1);
+                offerers[i].products.splice(j, 1)
             }
         }
     }
 }
 
-var offerers = [];
+var offerers = []
